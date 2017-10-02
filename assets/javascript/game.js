8d761577ac56ef30220a2c1d6d7987ef6b75be96
Peter Peck <karatekid4christ@gmail.com>
2017-10-01 21:54:43 -0500

    Final Homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,129 +1,94 @@
+$(document).ready(function() {
 // Global Variables for random number
+var gemOneValue, gemTwoValue, gemThreeValue, gemFourValue;
 var wins = 0;
 var loses = 0;
-var counter = 0;
+var userTallyScore;
+var crystalvalue;
 
-var crystalvalue = Math.floor(Math.random() * 50) + 19;
-var randomnumber1 = Math.floor(Math.random() * 12) + 1;
-var randomnumber2 = Math.floor(Math.random() * 12) + 1;
-var randomnumber3 = Math.floor(Math.random() * 12) + 1;
-var randomnumber4 = Math.floor(Math.random() * 12) + 1;
 
   //In puts target number into the HTML
-  $("#chosennumber").text("Random Number:  " + crystalvalue);
-  $("#totalpoints").text("Total Points:  " + counter);
   
   // Function startgame is defined
-    function startgame() {
-      
-      counter = 0;
-    	$(".blue").on("click", function(){
-          counter += randomnumber1;
-    		$("#totalpoints").text("Total Points:  " + counter);
+   function initializevariables() {
 
-        // Check Numbers
-      if(counter === crystalvalue) {
+	crystalvalue =Math.floor(Math.random() * 120) + 19;
+
+   	gemOneValue = Math.floor(Math.random() * 12 + 1);
+
+	gemTwoValue = Math.floor(Math.random() * 12 + 1);
+
+	gemThreeValue = Math.floor(Math.random() * 12 + 1);
+
+	gemFourValue = Math.floor(Math.random() * 12 + 1);
+
+	userTallyScore = 0;
+
+   	$("#chosennumber").html("Random Number:  " + crystalvalue);
+   	$("#totalpoints").html("Total Points:  " + userTallyScore);
+
+  	};
+
+  	function wonorlost() {
+      if(userTallyScore == crystalvalue) {
         console.log("You Win");
         wins++;
         $("#wins").text("Wins:  " + wins);
         alert("You Win");
-        counter = 0;
-        
+        initializevariables();
       }
 
-      else if(counter >=crystalvalue) {
+      else if(userTallyScore >crystalvalue) {
         console.log("you loose");
         loses++;
         $("#loses").text("Loses:  " + loses);
         alert("loose");
-        counter = 0;
+        initializevariables();
       }
-
-    	});
-
-
-
-
-
-      $(".red").on("click", function(){
-          counter += randomnumber2;
-        $("#totalpoints").text("Total Points:  " + counter);
-
-        // Check Numbers
-      if(counter === crystalvalue) {
-        console.log("You Win");
-        wins++;
-        $("#wins").text("Wins:  " + wins);
-        alert("You Win");
-        counter = 0;
       }
-
-      else if(counter >=crystalvalue) {
-        console.log("you loose");
-        loses++;
-        $("#loses").text("Loses:  " + loses);
-        alert("loose");
-        counter = 0;
-      }
-
-      });
-
-
-
-
-
-      $(".purple").on("click", function(){
-        counter += randomnumber3;
-        $("#totalpoints").text("Total Points:  " + counter);
-
-        // Check Numbers
-      if(counter === crystalvalue) {
-        console.log("You Win");
-        wins++;
-        $("#wins").text("Wins:  " + wins);
-        alert("You Win");
-        counter = 0;
-      }
-
-      else if(counter >=crystalvalue) {
-        console.log("you loose");
-        loses++;
-        $("#loses").text("Loses:  " + loses);
-        alert("loose");
-        counter = 0;
-        
-      }
-
-      });
-
-
-
-      
-
-      $(".yellow").on("click", function(){
-         counter += randomnumber4;
-        $("#totalpoints").text("Total Points:  " + counter);
-
-        // Check Numbers
-      if(counter === crystalvalue) {
-        console.log("You Win");
-        wins++;
-        $("#wins").text("Wins:  " + wins);
-        alert("You Win");
-        counter = 0;
-      }
-
-      else if(counter >=crystalvalue) {
-        console.log("you loose");
-        loses++;
-        $("#loses").text("Loses:  " + loses);
-        alert("loose");
-        counter = 0;
-      }
-
-      });
-    }
+   	
+   	initializevariables();
   
+    $(".gem").on("click", function() {
+		// each gem has a value attribute of gem1, gem2, gem3, or gem 4
+		// use this attribute to identify which gem the user actually clicked
+		var pressed = $(this).attr("value");
+
+        console.log(pressed);
+        if (pressed == "gem1") {
+        	userTallyScore += gemOneValue;
+        } 
+        else if (pressed == "gem2") {
+        	userTallyScore += gemTwoValue;
+        } 
+        else if (pressed == "gem3") {
+        	userTallyScore += gemThreeValue;
+        } 
+        else if (pressed == "gem4") {
+        	userTallyScore += gemFourValue;
+        } 
+        else {
+        	console.log("error");
+        }
+        // then update the html for the user score
+  		$("#totalpoints").html("Total Points:  " + userTallyScore);
+
+  		wonorlost();
+});
+    });
+
+
+
+
+
+
+
+
+
+        // Check Numbers
+      
+
+    
 // Calls startgame function and excutes code
-startgame()
+
 

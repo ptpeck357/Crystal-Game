06d3807d44a134c98391731e847f509bf348d759
Peter Peck <karatekid4christ@gmail.com>
2017-10-01 23:35:44 -0500

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-// Global Variables for random number
+
+// Global Variables 
 var gemOneValue, gemTwoValue, gemThreeValue, gemFourValue;
 var wins = 0;
 var loses = 0;
@@ -7,11 +7,12 @@ var userTallyScore;
 var crystalvalue;
 
 
-  //In puts target number into the HTML
   
-  // Function startgame is defined
+  
+  // Function "initializevariables" is defined
    function initializevariables() {
 
+   	//Generates random number for each crystal and target crystal
 	crystalvalue =Math.floor(Math.random() * 120) + 19;
 
    	gemOneValue = Math.floor(Math.random() * 12 + 1);
@@ -22,19 +23,24 @@ var crystalvalue;
 
 	gemFourValue = Math.floor(Math.random() * 12 + 1);
 
+	//Sets tally score back to zero every time the round starts over
 	userTallyScore = 0;
 
+	//In puts target number into the HTML
    	$("#chosennumber").html("Random Number:  " + crystalvalue);
    	$("#totalpoints").html("Total Points:  " + userTallyScore);
 
   	};
 
+  	//Function "wonorlost" is defined
   	function wonorlost() {
       if(userTallyScore == crystalvalue) {
         console.log("You Win");
         wins++;
         $("#wins").text("Wins:  " + wins);
         alert("You Win");
+
+        //Calls "initializevariables" function to start a new round
         initializevariables();
       }
 
@@ -42,53 +48,45 @@ var crystalvalue;
         console.log("you loose");
         loses++;
         $("#loses").text("Loses:  " + loses);
-        alert("loose");
+        alert("You lost");
+
+		//Calls "initializevariables" function to start a new round
         initializevariables();
       }
-      }
+    };
    	
+   	//Calls initializevariables function to begin the game
    	initializevariables();
   
+  	//On click listener. Each crystal has a value attribute of gem1, gem2, gem3, or crystal 4
     $(".gem").on("click", function() {
-		// each gem has a value attribute of gem1, gem2, gem3, or gem 4
-		// use this attribute to identify which gem the user actually clicked
+
+		// use this attribute to identify which crystal the user actually clicked
 		var pressed = $(this).attr("value");
 
+		//Compare to the current crystal that was clicked to the target crystal
         console.log(pressed);
         if (pressed == "gem1") {
         	userTallyScore += gemOneValue;
         } 
+
         else if (pressed == "gem2") {
         	userTallyScore += gemTwoValue;
         } 
+
         else if (pressed == "gem3") {
         	userTallyScore += gemThreeValue;
         } 
+
         else if (pressed == "gem4") {
         	userTallyScore += gemFourValue;
         } 
-        else {
-        	console.log("error");
-        }
-        // then update the html for the user score
+        
+        // then update the html for the user tally score
   		$("#totalpoints").html("Total Points:  " + userTallyScore);
 
+  		//Calls the "wonorlost" function everytime when a crystal is clicked
   		wonorlost();
-});
-    });
-
-
-
-
-
-
-
-
-
-        // Check Numbers
-      
-
-    
-// Calls startgame function and excutes code
-
+	});
+  
 
